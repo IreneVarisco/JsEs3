de581fce9f463d64383333145bd9add302080a4c
BloodNiala <73111808+BloodNiala@users.noreply.github.com>
2020-12-10 23:26:15 +0100

    ok, ora per davvero funziona tutto

--- JsEs3.js
+++ JsEs3.js
@@ -1,4 +1,3 @@
-  var idd;
 
   $("#btn1").click(function () {
     calcola("+");
@@ -12,30 +11,40 @@
   $("#btn4").click(function () {
     calcola("*");
   });
-  function cancella(idd) {
-    $('#idd').remove();
+  function cancella() {
+    $("#tabella").on('click','.btnDelete',function(){
+      $(this).closest('tr').remove();
+    });
+  }
+  function tabella(n1, segno, n2, isco) {
+    $("#tabella").append("<tr><td>"+ n1 +"</td>"+"<td>"+ segno +"</td>"+"<td>"+ n2 +"</td>"+"<td>"+ isco +"</td> <td>"+ '<button class="btnDelete" style="background-color: crimson" onclick="cancella()">cancella</button>' +"</td><tr>");
+    
   }
   function calcola(segno) {
     var n1 = parseInt($("#num1").val());
     var n2 = parseInt($("#num2").val());
     var isco;
+    
     switch (segno) {
       case "+":
         isco = n1 + n2;
+        tabella(n1, segno, n2, isco);
         break;
       case "-":
         isco = n1 - n2;
+        tabella(n1, segno, n2, isco);
         break;
       case "/":
         isco = n1 / n2;
+        tabella(n1, segno, n2, isco);
         break;
       case "*":
         isco = n1 * n2;
+        tabella(n1, segno, n2, isco);
         break;
     }
     $("#span1").html(isco);
-    idd=""+n1+segno+n2;
-    $("#tabella").append("<tr id=idd><td>"+ n1 +"</td>"+"<td>"+ segno +"</td>"+"<td>"+ n2 +"</td>"+"<td>"+ isco +"</td>"+"<td>"+ '<button id="btn5" style="background-color: crimson" onclick="cancella(idd)">cancella</button>' +"</td><tr>");
+    
    
     
   }
