fe13749f9d3461c3e5d842bb5f18261c76b21bec
Varisco Irene <varisco.irene@osdbsesto>
2020-12-10 13:40:57 +0100

    tanti fail

--- JsEs3.js
+++ JsEs3.js
@@ -1,3 +1,5 @@
+  var idd;
+
   $("#btn1").click(function () {
     calcola("+");
   });
@@ -10,7 +12,6 @@
   $("#btn4").click(function () {
     calcola("*");
   });
-
   function calcola(segno) {
     var n1 = parseInt($("#num1").val());
     var n2 = parseInt($("#num2").val());
@@ -29,5 +30,10 @@
         isco = n1 * n2;
         break;
     }
+    idd=n1+segno+n2;
     $("#span1").html(isco);
+    $("#tabella").append("<tr id=idd><td>"+ n1 +"</td>"+"<td>"+ segno +"</td>"+"<td>"+ n2 +"</td>"+"<td>"+ isco +"</td>"+"<td>"+ $("#btn5").click(function () {
+      cancella(idd);
+    }) +"</td><tr>");
+    console.log(n1 + segno + n2);
   }
