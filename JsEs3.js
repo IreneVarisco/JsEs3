6633c542f11f5c8998a3aac0a549bc13043a44eb
Varisco Irene <varisco.irene@osdbsesto>
2021-01-07 13:39:39 +0100

    waa

--- JsEs3.js
+++ JsEs3.js
@@ -17,14 +17,19 @@
     });
   }
   function tabella(n1, segno, n2, isco) {
-    $("#tabella").append("<tr><td>"+ n1 +"</td>"+"<td>"+ segno +"</td>"+"<td>"+ n2 +"</td>"+"<td>"+ isco +"</td> <td>"+ '<button class="btnDelete" style="background-color: crimson" onclick="cancella()">cancella</button>' +"</td><tr>");
     
+    var string = ("<tr><td>"+ n1 +"</td>"+"<td>"+ segno +"</td>"+"<td>"+ n2 +"</td>"+"<td>"+ isco +"</td> <td>"+ '<button class="btnDelete" style="background-color: crimson" onclick="cancella()">cancella</button>' +"</td><tr>");
+    var line = $(string);
+    localStorage.setItem("operazione", string);
+    $("#tabella").append(line);
+
+    var last_operation = localStorage.getItem("operazione");
   }
   function calcola(segno) {
     var n1 = parseInt($("#num1").val());
     var n2 = parseInt($("#num2").val());
     var isco;
-    
+
     switch (segno) {
       case "+":
         isco = n1 + n2;
